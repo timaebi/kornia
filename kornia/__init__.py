84d3295b7e6f29ce3c7ced25950723d9455b6d02
timaebi <tim.aebi@gmail.com>
2019-08-26 09:41:10 +0200

    remove check for python 3.6

--- kornia/__init__.py
+++ kornia/__init__.py
@@ -1,8 +1,7 @@
 # Make sure that kornia is running on Python 3.6.0 or later
 # (to avoid running into this bug: https://bugs.python.org/issue29246)
 import sys
-if sys.version_info < (3, 6, 0):
-    raise RuntimeError("Kornia requires Python 3.6.0 or later")
+
 
 from .version import __version__
 
